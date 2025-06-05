bfbc1900d29ad7b88eb50506447e487639b0c6a2
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-06-05 20:24:22 +0330

    Addition Falling platform with its impact animation and different swing movement using c# code

--- Assets/Scripts/Traps/Trap_FallingPlatform.cs
+++ Assets/Scripts/Traps/Trap_FallingPlatform.cs
@@ -1,24 +1,58 @@
+using System.Collections;
 using UnityEngine;
 
 public class Trap_FallingPlatform : MonoBehaviour
 {
     //We don't use inheritance here, but we can use it if we want to (There is no reason to use for one method and one variable)
 
+    private Animator anim;
+    private Rigidbody2D rb;
+    private BoxCollider2D boxCollider;
+    private CapsuleCollider2D capsuleCollider;
+
+
     [SerializeField] private float speed = 1f;
     [SerializeField] private float travelDistance;
 
+
+    [Header("Platform Fall Detail")]
+    [SerializeField] private float impactSpeed = 3; // For how much does the platform is moved down by the player
+    [SerializeField] private float impactDuration = .1f; // For how long the platform is impacted by the player
+    [SerializeField] private float powerImpact = 10f;
+    private float impactTimer;
+    private bool impactHapped; // To prevent the player pushing the platform down all the time (OR it shows the player that it will fall soon)
+
+    [Space]
+    [SerializeField] private float fallDelay = 1f;
+
     private  Vector3[] waypoints;
     private int wayPointIndex;
-    public bool canMove;
+    private bool canMove =  false;
 
 
-    private void Start()
+    private void Awake()
     {
-        StartWayPoint();
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        capsuleCollider = GetComponent<CapsuleCollider2D>();
+    }
+
+    private IEnumerator Start()
+    {
+        StartWayPoint();// For the platform moving up and down, feeling of floating in the air
+
+
+        float startDelay = Random.Range(0, 0.6f);//actually form 0 to 0.59
+        
+        yield return new WaitForSeconds(startDelay);
+
+        canMove = true;
     }
 
     private void Update()
     {
+        HandleImpact();
         HandleMovement();
     }
 
@@ -34,7 +68,7 @@ public class Trap_FallingPlatform : MonoBehaviour
 
     private void HandleMovement()
     {
-        if (!canMove)
+        if (!canMove)// one logic here is when this becomes true, the deactivated plaftform won't be handled by the following commands.
             return;
 
         //I had a problem here! I didn't assing this to the postion of the transform, so it didn't move
@@ -47,5 +81,55 @@ public class Trap_FallingPlatform : MonoBehaviour
             if(wayPointIndex >= waypoints.Length)
                 wayPointIndex = 0;
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (impactHapped)
+            return;
+
+        Player player = collision.gameObject.GetComponent<Player>();
+
+        if (player != null)
+        {
+            Invoke(nameof(SwithOffpltform), fallDelay);
+
+            impactTimer = impactDuration;
+
+            impactHapped = true;
+            //Debug.Log("Player is on falling platform");
+        }
+    }
+
+    //We move the platform down by a timer while the player jumps on the platform
+    private void HandleImpact()
+    {
+        if (impactTimer < 0)
+            return;
+
+        impactTimer -= Time.deltaTime;
+
+        transform.position = Vector2.MoveTowards(transform.position, (Vector3.down * powerImpact), impactSpeed * Time.deltaTime);
+    }
+
+
+    //this method should be called after a delay. so it won't fall when the player stands on it immediately
+    private void SwithOffpltform()
+    {
+        canMove = false; //To prevent jitter effect of the deactivated platform
+
+        anim.SetTrigger("deactive");
+
+        //rb.isKinematic = false;   THIS IS OBSOLETE IN UNITY 6
+        rb.bodyType = RigidbodyType2D.Dynamic; // The new version
+
+        rb.gravityScale = 2.5f;
+        
+        //rb.drag = .3f; THIS ONE IS OBSOLETE TOO
+        rb.linearDamping = .5f; //The new version
+
+
+        boxCollider.enabled = false;
+        capsuleCollider.enabled = false;
     }
 }
