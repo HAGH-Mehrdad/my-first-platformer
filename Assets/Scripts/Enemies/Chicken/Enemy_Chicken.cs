b1b3757ec0f1d46758bec36fe999ca1bcc5c54cd
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-06-18 08:30:17 +0330

    Addition Enemy-Chicken simple setup [overring Enemy.cs and EnemyMushroom_AC animation controller] so it behaves exactly like Mushroom

---
+++ Assets/Scripts/Enemies/Chicken/Enemy_Chicken.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+public class Enemy_Chicken : Enemy
+{
+    protected override void Update()
+    {
+        base.Update();
+
+        HandleAnimation();
+
+        if (isDead)
+            return;
+
+        HandleCollision();
+        HandleMovement();
+        HandleTurnAround();
+    }
+
+    private void HandleTurnAround()
+    {
+        //we can check this condition on update method but due to Encapsulation, readability and flexibility we check isGrounded here.
+        if (!isGrounded)
+            return; // If the enemy is not grounded, do not flip [When the designer didn't place the enemy on the ground before play]
+
+        if (!isGroundAheadDetected || isWallDetected)
+        {
+            Flip();
+            idleTimer = idleDuration; // Reset the idle timer when the enemy is not grounded or hits a wall (wants to flip!)
+            rb.linearVelocityX = 0; // Stop moving when the enemy is not grounded or hits a wall
+        }
+    }
+
+    private void HandleMovement()
+    {
+        if (idleTimer > 0)
+            return; // If the idle [animation] timer is not finished, do not move
+
+        if (isGroundAheadDetected)
+            rb.linearVelocity = new Vector2(moveSpeed * facingDir, rb.linearVelocityY);//this condition removes jittering effect when the player is falling from a platform or is simply not on the ground
+    }
+
+    private void HandleAnimation()
+    {
+        anim.SetFloat("xVelocity", rb.linearVelocityX);
+    }
+}
