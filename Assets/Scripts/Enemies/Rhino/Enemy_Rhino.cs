24be80d4e2826794af4a2faef57112c7a160cfc2
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-06-25 08:19:05 +0330

    addition Rhino script/ copying chicken player detection and adding their fields

---
+++ Assets/Scripts/Enemies/Rhino/Enemy_Rhino.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Enemy_Rhino : Enemy
+{
+    [SerializeField] private float detectionRange;
+    
+    
+    private bool playerDetected;
+
+    protected override void HandleCollision()
+    {
+        base.HandleCollision();
+
+        playerDetected = Physics2D.Raycast(transform.position, Vector2.right * facingDir, detectionRange, whatIsPlayer);
+    }
+
+    protected override void OnDrawGizmos()
+    {
+        base.OnDrawGizmos();
+        Gizmos.DrawLine(transform.position, new Vector2(transform.position.x + (detectionRange * facingDir), transform.position.y));
+    }
+}
