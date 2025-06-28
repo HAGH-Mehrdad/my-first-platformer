f07825d0e6e47ef541ec009210e3516a8f9f230e
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-06-28 17:38:37 +0330

    addition a routine of shooting bullets.

--- Assets/Scripts/Enemies/Plant/Enemy_Plant.cs
+++ Assets/Scripts/Enemies/Plant/Enemy_Plant.cs
@@ -2,17 +2,34 @@ using UnityEngine;
 
 public class Enemy_Plant : Enemy
 {
+
+    [Header("Plant details")]
+    [SerializeField] private float attackCooldown = 2f; // Time between attacks
+    private float lastTimeAttacked;
+
+
     protected override void Update()
     {
         base.Update();
 
-        if (isPlayerDetected)
+        bool canAttack = Time.time >= lastTimeAttacked + attackCooldown;// to update the time to decide if the plant can attack or not
+
+        if (isPlayerDetected && canAttack)
             Attack();
     }
 
 
     private void Attack()
     {
+        lastTimeAttacked = Time.time; //To keep track of the last attack time
+
         anim.SetTrigger("attack");
+    }
+
+
+    protected override void HandleAnimation()
+    {
+        //Due to static nature of the plant enemy, we don't need to update the animation based on velocity [By not calling base mothod]
+        //keep this empty unless you want to add specific animations for the plant enemy
     }
 }
