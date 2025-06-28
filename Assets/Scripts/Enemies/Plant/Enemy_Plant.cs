159315a2b69400f721a3841de03330f7307f58c4
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-06-28 15:44:52 +0330

    addition simple attack routine. [attack speed is high. probably needs a delay attack]./this enemy does not need to move so ground check is not impprtant here.

--- Assets/Scripts/Enemies/Plant/Enemy_Plant.cs
+++ Assets/Scripts/Enemies/Plant/Enemy_Plant.cs
@@ -2,5 +2,17 @@ using UnityEngine;
 
 public class Enemy_Plant : Enemy
 {
+    protected override void Update()
+    {
+        base.Update();
 
+        if (isPlayerDetected)
+            Attack();
+    }
+
+
+    private void Attack()
+    {
+        anim.SetTrigger("attack");
+    }
 }
