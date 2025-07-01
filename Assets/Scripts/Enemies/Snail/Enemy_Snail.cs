ce5508903316da84e1db3fe1cdc2eb4af1f46406
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-07-01 22:18:08 +0330

    Addition, enemy snail script in which it is like mushroom but has hasBody state to indicate whether it can it can go to shell state or die after hitting twice.

---
+++ Assets/Scripts/Enemies/Snail/Enemy_Snail.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class Enemy_Snail : Enemy
+{
+    private bool hasBody = true;
+    protected override void Update()
+    {
+        base.Update();
+
+        if (isDead)
+            return;
+
+        HandleMovement();
+        HandleTurnAround();
+    }
+
+    private void HandleTurnAround()
+    {
+        //we can check this condition on update method but due to Encapsulation, readability and flexibility we check isGrounded here.
+        if (!isGrounded)
+            return; // If the enemy is not grounded, do not flip [When the designer didn't place the enemy on the ground before play]
+
+        if (!isGroundAheadDetected || isWallDetected)
+        {
+            Flip();
+            idleTimer = idleDuration; // Reset the idle timer when the enemy is not grounded or hits a wall (wants to flip!)
+            rb.linearVelocityX = 0; // Stop moving when the enemy is not grounded or hits a wall
+        }
+    }
+
+    public override void Die()
+    {
+        if (hasBody)
+        {
+            canMove = false;
+            hasBody = false;
+
+            anim.SetTrigger("hit");
+
+            idleTimer = 0; // prevent the shell to wait after hitting a wall.(it will bounce between walls none stop)
+            rb.linearVelocity = Vector2.zero; // Stop moving when the snail is hit
+        }
+        else
+        {
+            base.Die();
+        }
+
+    }
+
+    private void HandleMovement()
+    {
+        if (idleTimer > 0)
+            return; // If the idle [animation] timer is not finished, do not move
+
+        if(canMove == false)
+            return; // If the snail is dead, do not move. THE SHELL STILL THERE.
+
+        if (isGroundAheadDetected)//this condition removes jittering effect when the player is falling from a platform or is simply not on the ground
+            rb.linearVelocity = new Vector2(moveSpeed * facingDir, rb.linearVelocityY);
+    }
+}
