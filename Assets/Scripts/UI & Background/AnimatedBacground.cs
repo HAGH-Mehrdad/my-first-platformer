ea5cdecf95a61c321b79fa76d39f800984babb6c
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-07-03 20:27:39 +0330

    addition, background types to the script where the designer can change then.

--- Assets/Scripts/UI & Background/AnimatedBacground.cs
+++ Assets/Scripts/UI & Background/AnimatedBacground.cs
@@ -8,14 +8,27 @@ public class AnimatedBacground : MonoBehaviour
     private MeshRenderer mesh;
 
 
+    [Header("Background Type")]
+    [SerializeField] private BackgroundType backgroundType; // This is used to set the type of background we want to use, it is stored in BackgroundType.cs
+
+    [SerializeField] private Texture2D[] textures; 
+
     private void Awake()
     {
         mesh = GetComponent<MeshRenderer>();
+        UpdateBackgroundTexture();
     }
 
 
     private void Update()
     {
         mesh.material.mainTextureOffset += movementDirection * Time.deltaTime; // by changing the offset of the image material we create frame reate animated background
+    }
+
+
+    [ContextMenu ("Update Background")]
+    private void UpdateBackgroundTexture()
+    {
+        mesh.material.mainTexture = textures[(int)backgroundType];
     }
 }
