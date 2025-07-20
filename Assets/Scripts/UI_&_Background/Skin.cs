1296172f864b62a8b550d41d4dd1fc76beedfd2c
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-07-20 18:56:15 +0330

    addition, udating skin price according to their price.

---
+++ Assets/Scripts/UI_&_Background/Skin.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+
+[System.Serializable]
+public struct Skin
+{
+    public string skinName;
+    public int skinPrice;
+    public bool unlocked;
+
+}
--- Assets/Scripts/UI_&_Background/UI_SkinSelection.cs
+++ Assets/Scripts/UI_&_Background/UI_SkinSelection.cs
@@ -1,25 +1,55 @@
+using TMPro;
+using Unity.Collections;
 using UnityEngine;
 
 public class UI_SkinSelection : MonoBehaviour
 {
-    [SerializeField] private int currentIndex;
+
+    [SerializeField] private Skin[] skinList;
+
+    [Header("UI details")]
+    [SerializeField] private int skinIndex;
     [SerializeField] private int maxIndex;// depending on how many characters we have minus one
     [SerializeField] private Animator skinDisplay;
 
+    [Header("txt Info")]
+    [SerializeField] private TextMeshProUGUI txtBuy_Select;
+    [SerializeField] private TextMeshProUGUI txtPrice;
+    [SerializeField] private TextMeshProUGUI txtBank;
 
+    private void Start()
+    {
+        LoadSkinUnlocks();
+        UpdateSkinDisplay();
+    }
+
+    private void LoadSkinUnlocks()
+    {
+        for (int i = 1; i < skinList.Length; i++)//starts at one because no matter what the first skin is unlocked
+        {
+            string skinName = skinList[i].skinName;
+            bool skinUnlocked = PlayerPrefs.GetInt(skinName + "Unlocked", 0) == 1;
+
+            if(skinUnlocked)
+                skinList[i].unlocked = true; // if the skin is unlocked, set it to true in the skinList
+        }
+    }
 
     public void SelectSkin()
     {
-        SkinManager.instance.SetSkinId(currentIndex);
+        if (skinList[skinIndex].unlocked == false)
+            BuySkin(skinIndex);
+        else
+            SkinManager.instance.SetSkinId(skinIndex);
     }
 
     public void NextSkin()
     {
-        currentIndex++;
+        skinIndex++;
 
-        if (currentIndex > maxIndex)
+        if (skinIndex > maxIndex)
         {
-            currentIndex = 0;//scoll between skins endlessly.
+            skinIndex = 0;//scoll between skins endlessly.
         }
 
         UpdateSkinDisplay();
@@ -28,11 +58,11 @@ public class UI_SkinSelection : MonoBehaviour
 
     public void PreviousSkin()
     {
-        currentIndex--;
+        skinIndex--;
 
-        if (currentIndex < 0)
+        if (skinIndex < 0)
         {
-            currentIndex = maxIndex;//scoll between skins endlessly.
+            skinIndex = maxIndex;//scoll between skins endlessly.
         }
 
         UpdateSkinDisplay();
@@ -40,11 +70,37 @@ public class UI_SkinSelection : MonoBehaviour
 
     private void UpdateSkinDisplay()
     {
+        txtBank.text = "Bank: " + FruitsInBank(); //display the amount of fruits in the bank
+
+
         for (int i = 0; i < skinDisplay.layerCount; i++)
         {
             skinDisplay.SetLayerWeight(i, 0);
         }
 
-        skinDisplay.SetLayerWeight(currentIndex, 1);
+        skinDisplay.SetLayerWeight(skinIndex, 1);
+
+        if (skinList[skinIndex].unlocked)
+        {
+            txtPrice.transform.parent.gameObject.SetActive(false); // disable entire the price text object
+            txtBuy_Select.text = "Select";
+        }
+        else
+        {
+            txtPrice.transform.parent.gameObject.SetActive(true); // enable entire the price text object
+            txtPrice.text = "Price: " + skinList[skinIndex].skinPrice; //display the price of the skin
+            txtBuy_Select.text = "Buy";
+        }
     }
+
+    private void BuySkin(int index)// the functionality is save the information about the skin
+    {
+        string skinName = skinList[skinIndex].skinName;
+        skinList[skinIndex].unlocked = true; // unlock the skin
+
+        PlayerPrefs.SetInt(skinName + "Unlocked", 1); // save the skin as unlocked in PlayerPrefs
+    }
+
+
+    private int FruitsInBank() => PlayerPrefs.GetInt("TotalFruitsAmount");
 }
