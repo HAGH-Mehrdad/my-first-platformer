39855f201595597288d3952831cbb73248d98cdf
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-07-25 18:35:56 +0330

    addition, pause menu plus its funtionality with escape button [without the pause button for mobiles].

--- Assets/Scripts/UI_&_Background/UI_InGame.cs
+++ Assets/Scripts/UI_&_Background/UI_InGame.cs
@@ -1,6 +1,7 @@
 using TMPro;
 using UnityEditor.PackageManager;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class UI_InGame : MonoBehaviour
 {
@@ -13,6 +14,11 @@ public class UI_InGame : MonoBehaviour
     [SerializeField] private TextMeshProUGUI timerTxt;// Text component to display the timer
     [SerializeField] private TextMeshProUGUI fruitTxt;// Text component to display the fruit count
 
+    [Header("Pause menu reference")]
+    [SerializeField] private GameObject pauseMenu; // Reference to the pause menu UI
+
+    private bool isPaused = false; // Flag to check if the game is paused
+
     private void Awake()
     {
         instance = this;
@@ -23,6 +29,45 @@ public class UI_InGame : MonoBehaviour
     private void Start()
     {
         fadeEffect.ScreenFade(0, fadeDuration);
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseButtonPressed();
+        }
+    }
+
+    public void PauseButtonPressed()
+    {
+        if (isPaused)
+        {
+            isPaused = false;
+            Time.timeScale = 1f; // Resume the game
+            pauseMenu.SetActive(false); // Hide the pause menu
+        }
+        else
+        {
+            isPaused = true;
+            Time.timeScale = 0f; // Pause the game
+            pauseMenu.SetActive(true); // Show the pause menu
+        }
+    }
+
+    public void ResumeButton()
+    {
+        isPaused = false; // Set the pause flag to false
+        pauseMenu.SetActive(false); // Hide the pause menu
+        Time.timeScale = 1f; // Resume the game
+    }
+
+    public void MainMenuButton()
+    {
+        isPaused = false; // Set the pause flag to false
+        pauseMenu.SetActive(false); // Hide the pause menu
+        SceneManager.LoadScene("MainMenu"); // Load the main menu scene
+        Time.timeScale = 1f; // Ensure the game is resumed when returning to the main menu
     }
 
     public void UpdateFruitUI(int collectedFruits, int totalFruits)
