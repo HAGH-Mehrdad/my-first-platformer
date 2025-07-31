6b65691717fff003f2c0920e020c07ea230aecea
DESKTOP-4V9IPU8\Packman <mehrdad1994@gmail.com>
2025-07-31 19:33:47 +0330

    addition, handling facing direction when moving to the skin selection and moving back to the menu.

--- Assets/Scripts/Camera/MenuCharacter.cs
+++ Assets/Scripts/Camera/MenuCharacter.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.Windows;
 
 public class MenuCharacter : MonoBehaviour
 {
@@ -7,7 +8,11 @@ public class MenuCharacter : MonoBehaviour
     [SerializeField] private float speed;
 
     private Vector3 destinationPoint;
+
+
     private bool isMoving;
+    private int facingDir = 1;
+    private bool facingRight = true;
 
     private void Awake()
     {
@@ -37,6 +42,21 @@ public class MenuCharacter : MonoBehaviour
         destinationPoint = newDestination.position;
         destinationPoint.y = newDestination.position.y;  // to igonre y-axis movement between the player and the game object that is planed for the destination point
 
+        HandleFlip(destinationPoint.x);
         isMoving = true;
+    }
+
+    private void HandleFlip(float xValue)
+    {
+        //if the player is running to left and facing to right OR if it is running to right and facing to left we need to flip the character.
+        if (xValue < transform.position.x && facingRight || xValue > transform.position.x && !facingRight)
+            Flip();
+    }
+
+    private void Flip()
+    {
+        facingDir *= -1;
+        transform.Rotate(0, 180, 0);
+        facingRight = !facingRight;
     }
 }
